4614d2934dc17701b4594fd7c006c0b73c647dfc
Belgacem Khadher <devbekh@gmail.com>
2024-04-24 19:18:02 +0100

    Online code editor

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function run(){
+    let htmlCode = document.getElementById('html-code').value;
+    let cssCode = document.getElementById('css-code').value;
+    let jsCode = document.getElementById('js-code').value;
+    let output = document.getElementById('output');
+
+    output.contentDocument.body.innerHTML = htmlCode+"<style>"+cssCode+"</style>";
+    output.contentWindow.eval(jsCode)
+}
+
 //3.Write a function called returnFive that takes no parameters and returns the number 5 when invoked. HINT: Use “console.log()”.
 
 
